0fbd924a594b3d2e3942333f017a912daa9a5f48
Sajjadalgburi <dfgsajjad@gmail.com>
2024-01-16 12:46:15 -0700

    fetching prayer times using the users ip adress as paramateres

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -12,3 +12,20 @@ async function getUserLocation() {
     return null;
   }
 }
+
+// Function to fetch prayer timings using the user's location
+async function fetchPrayerTimings() {
+  const userLocation = await getUserLocation();
+
+  if (userLocation) {
+    const apiUrl = `http://api.aladhan.com/v1/timingsByCity?city=${userLocation.city}&country=${userLocation.country}&method=8`;
+
+    // Now you can use the apiUrl to fetch prayer timings
+    // Example: fetch(apiUrl).then(response => response.json()).then(data => console.log(data));
+  } else {
+    console.error("Unable to determine user location.");
+  }
+}
+
+// Call the function to fetch prayer timings
+fetchPrayerTimings();
