83160bd2188b9e86f038fc0e80a1e97f3a3b23be
Sajjadalgburi <dfgsajjad@gmail.com>
2024-01-16 13:12:04 -0700

    added api key and added fetch request from prayer times API

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,7 +1,9 @@
+const ApiKey = "b41fb0ef272b35"; // Replace with your actual API key
+
 // Function to get user's location based on IP address
 async function getUserLocation() {
   try {
-    const response = await fetch("https://ipinfo.io/json");
+    const response = await fetch(`https://ipinfo.io/json?token=${ApiKey}`);
     const data = await response.json();
     return {
       city: data.city,
@@ -18,10 +20,16 @@ async function fetchPrayerTimings() {
   const userLocation = await getUserLocation();
 
   if (userLocation) {
-    const apiUrl = `http://api.aladhan.com/v1/timingsByCity?city=${userLocation.city}&country=${userLocation.country}&method=8`;
+    const apiUrl = `http://api.aladhan.com/v1/timingsByCity?city=${userLocation.city}&country=${userLocation.country}&method=2`;
 
-    // Now you can use the apiUrl to fetch prayer timings
-    // Example: fetch(apiUrl).then(response => response.json()).then(data => console.log(data));
+    console.log(apiUrl);
+
+    // Make fetch request
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   } else {
     console.error("Unable to determine user location.");
   }
